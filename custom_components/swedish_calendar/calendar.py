aec0214c956d2bebb622a3691a32b1dc0afb5a1c
Magnus Larsson <maglars2@gmail.com>
2022-11-04 17:42:01 +0100

    Add calendar and fetch data for multiple dates

---
+++ custom_components/swedish_calendar/calendar.py
@@ -0,0 +1,108 @@
+from __future__ import annotations
+
+import logging
+from datetime import datetime, date, timedelta
+from typing import List, Optional, Any, Dict
+
+from homeassistant.components.calendar import CalendarEntity, CalendarEvent
+from homeassistant.core import callback, HomeAssistant
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from .const import DOMAIN, SENSOR_TYPES, CONF_CALENDAR, CONF_INCLUDE, DOMAIN_FRIENDLY_NAME
+from .provider import CalendarDataCoordinator
+from .types import SwedishCalendar, SensorConfig
+from .utils import DateUtils
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_platform(hass: HomeAssistant, config, async_add_entities, discovery_info=None) -> None:
+    """Set up the calendar sensor."""
+    coordinator = hass.data[DOMAIN]["coordinator"]
+    conf = hass.data[DOMAIN]["conf"][CONF_CALENDAR]
+
+    sensor_configs = [SENSOR_TYPES[sensor_type]
+                      for sensor_type in SENSOR_TYPES
+                      if sensor_type in conf[CONF_INCLUDE]]
+
+    if len(sensor_configs) > 0:
+        async_add_entities([SwedishCalendarEntity(sensor_configs, coordinator)])
+
+
+class SwedishCalendarEntity(CalendarEntity, CoordinatorEntity):
+    """Representation of a Demo Calendar element."""
+
+    def __init__(self, sensor_configs: List[SensorConfig], coordinator: CalendarDataCoordinator) -> None:
+        super().__init__(coordinator)
+        self._sensor_configs = sensor_configs
+        self._attr_name = DOMAIN_FRIENDLY_NAME
+        self._events: List[SwedishCalendarEvent] = []
+
+    @property
+    def should_poll(self):
+        return False
+
+    @property
+    def event(self) -> CalendarEvent:
+        """Return the next upcoming event."""
+        return self._events[0].to_hass_calendar_event() if len(self._events) > 0 else None
+
+    async def async_get_events(
+            self,
+            hass: HomeAssistant,
+            start_date: datetime,
+            end_date: datetime,
+    ) -> list[CalendarEvent]:
+        return [event.to_hass_calendar_event()
+                for event in self._events
+                if DateUtils.in_range(str(event.date), start_date.date(), end_date.date())
+                ]
+
+    async def async_added_to_hass(self):
+        self._handle_coordinator_update()  # Set initial state
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        swedish_calendars: Dict[date, SwedishCalendar] = self.coordinator.data
+        events = []
+        for event_date in swedish_calendars.keys():
+            swedish_calendar = swedish_calendars[event_date]
+
+            for sensor_config in self._sensor_configs:
+                sensor_value = sensor_config.get_value_from_calendar(swedish_calendar)
+                description = SwedishCalendarEntity._describe(sensor_value, sensor_config)
+                if description:
+                    event = SwedishCalendarEvent(isodate=event_date, description=description)
+                    events.append(event)
+
+        self._events = events
+
+    @staticmethod
+    def _describe(value: Any, item: SensorConfig) -> Any:
+        if value is None:
+            return None
+        elif isinstance(value, bool):
+            return SwedishCalendarEntity._describe_bool(value, item.friendly_name)
+        elif isinstance(value, int):
+            return SwedishCalendarEntity._describe_int(value, item.friendly_name)
+        else:
+            return value
+
+    @staticmethod
+    def _describe_bool(value: bool, friendly_name: str) -> Optional[str]:
+        return friendly_name if value else None
+
+    @staticmethod
+    def _describe_int(value: int, friendly_name: str) -> str:
+        return f'{friendly_name}: {value}'
+
+
+class SwedishCalendarEvent:
+    def __init__(self, isodate: date, description: str):
+        self.date = isodate
+        self.description = description
+
+    def to_hass_calendar_event(self) -> CalendarEvent:
+        end = self.date + timedelta(days=1)
+        return CalendarEvent(start=self.date, end=end, summary=self.description, description=self.description)
--- custom_components/swedish_calendar/sensor.py
+++ custom_components/swedish_calendar/sensor.py
@@ -5,7 +5,8 @@ For more details about this platform, please refer to the documentation at
 https://github.com/Miicroo/ha-swedish_calendar
 """
 import logging
-from typing import Any
+from datetime import date
+from typing import Any, Dict, List
 
 from homeassistant.const import ATTR_ATTRIBUTION
 from homeassistant.core import callback, HomeAssistant
@@ -14,7 +15,7 @@ from homeassistant.util import slugify
 
 from .const import DOMAIN, SENSOR_TYPES, CONF_EXCLUDE
 from .provider import CalendarDataCoordinator
-from .types import SwedishCalendar
+from .types import SensorConfig, SwedishCalendar
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -24,46 +25,34 @@ async def async_setup_platform(hass: HomeAssistant, config, async_add_entities, 
     coordinator = hass.data[DOMAIN]["coordinator"]
     conf = hass.data[DOMAIN]["conf"]
 
-    included_sensor_types = [sensor_type for sensor_type in SENSOR_TYPES if sensor_type not in conf[CONF_EXCLUDE]]
+    included_sensor_types: List[str] = [sensor_type
+                                                 for sensor_type in SENSOR_TYPES
+                                                 if sensor_type not in conf[CONF_EXCLUDE]]
 
-    devices = []
-    for sensor_type in included_sensor_types:
-        name = SENSOR_TYPES[sensor_type][0]
-        icon = SENSOR_TYPES[sensor_type][1]
-        state_key = SENSOR_TYPES[sensor_type][2]
-        default_value = SENSOR_TYPES[sensor_type][3]
-        attribution = SENSOR_TYPES[sensor_type][4]
-        devices.append(
-            SwedishCalendarSensor(sensor_type, name, icon, state_key, default_value, attribution, coordinator))
-
+    devices = [SwedishCalendarSensor(sensor_type, SENSOR_TYPES[sensor_type], coordinator)
+               for sensor_type in included_sensor_types]
     async_add_entities(devices)
 
 
 class SwedishCalendarSensor(CoordinatorEntity):
 
-    def __init__(self, sensor_type: str, name: str, icon: str, state_key: str, default_value: Any, attribution: str,
-                 coordinator: CalendarDataCoordinator):
+    def __init__(self, sensor_type: str, sensor_config: SensorConfig, coordinator: CalendarDataCoordinator):
         super().__init__(coordinator)
-        self.type = sensor_type
-        self._name = name
-        self._icon = icon
-        self.state_key = state_key
-        self._default_value = default_value
-        self._attribution = attribution
         self.entity_id = 'sensor.swedish_calendar_{}'.format(sensor_type)
+        self._sensor_config = sensor_config
         self._state = None
 
     @property
     def name(self):
-        return self._name
+        return self._sensor_config.friendly_name
 
     @property
     def unique_id(self):
-        return 'sensor.{}'.format(slugify(self._name))
+        return 'sensor.{}'.format(slugify(self._sensor_config.friendly_name))
 
     @property
     def state(self):
-        return self._state if self._state else self._default_value
+        return self._state if self._state else self._sensor_config.default_value
 
     @property
     def should_poll(self):
@@ -72,12 +61,12 @@ class SwedishCalendarSensor(CoordinatorEntity):
 
     @property
     def icon(self):
-        return self._icon
+        return self._sensor_config.icon
 
     @property
     def extra_state_attributes(self):
         return {
-            ATTR_ATTRIBUTION: self._attribution,
+            ATTR_ATTRIBUTION: self._sensor_config.attribution,
         }
 
     @property
@@ -96,9 +85,11 @@ class SwedishCalendarSensor(CoordinatorEntity):
     @callback
     def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator."""
-        swedish_calendar: SwedishCalendar = self.coordinator.data
-        if swedish_calendar is not None:
-            state = swedish_calendar.get_value_by_attribute(self.state_key)
+        swedish_calendars: Dict[date, SwedishCalendar] = self.coordinator.data
+        today = date.today()
+        if today in swedish_calendars:
+            swedish_calendar = swedish_calendars[today]
+            state = self._sensor_config.get_value_from_calendar(swedish_calendar)
             if isinstance(state, list):
                 state = ",".join(state)
             elif isinstance(state, bool):
