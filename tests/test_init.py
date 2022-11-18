9eb684085b5f99ab51310d8370beabd32d3e9e5c
Magnus Larsson <maglars2@gmail.com>
2022-11-18 16:35:54 +0100

    Add CI and tests

---
+++ tests/test_init.py
@@ -0,0 +1,9 @@
+"""Test component setup."""
+
+from custom_components.swedish_calendar.const import DOMAIN
+from homeassistant.setup import async_setup_component
+
+
+async def test_async_setup(hass):
+    """Test the component gets setup."""
+    assert await async_setup_component(hass, DOMAIN, {}) is True
