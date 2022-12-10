2f56cd81273819ae9108e34060dce5924c04c589
Magnus Larsson <maglars2@gmail.com>
2022-12-10 13:08:22 +0100

    Bugfix/33 Fix incorrect update time (#34)
    * Add tomorrow test case
    
    More test cases in test_seconds_until_midnight
    
    * Use input parameter from_time in seconds_until_midnight
    
    Add debug rows to test locally
    
    * Update format of test
    
    * Correct default value for seconds_until_midnight

--- custom_components/swedish_calendar/utils.py
+++ custom_components/swedish_calendar/utils.py
@@ -13,8 +13,10 @@ class DateUtils:
         return start <= date.fromisoformat(isodate) <= end
 
     @staticmethod
-    def seconds_until_midnight(from_time: datetime = datetime.now()) -> int:
-        tomorrow = date.today() + timedelta(days=1)
+    def seconds_until_midnight(from_time: datetime = None) -> int:
+        from_time = from_time or datetime.now()  # Use now() if no time provided
+        tomorrow = from_time + timedelta(days=1)
         midnight = datetime.combine(tomorrow, datetime.min.time())
         diff = midnight - from_time
+
         return diff.days*86400 + diff.seconds + 1
