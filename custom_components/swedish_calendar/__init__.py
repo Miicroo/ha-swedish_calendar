a563406baf128119c997f1067c9e5983f2341882
Magnus Larsson <maglars2@gmail.com>
2024-06-10 09:08:57 +0200

    Remove unused code

--- custom_components/swedish_calendar/__init__.py
+++ custom_components/swedish_calendar/__init__.py
@@ -39,10 +39,6 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(hass: HomeAssistant, entry: config_entries.ConfigEntry) -> bool:
     """Set up platform from a ConfigEntry."""
     conf = dict(entry.data)
-    # Registers update listener to update config entry when options are updated.
-    #unsub_options_update_listener = entry.add_update_listener(options_update_listener)
-    # Store a reference to the unsubscribe function to cleanup if an entry is unloaded.
-    #hass_data["unsub_options_update_listener"] = unsub_options_update_listener
 
     special_themes_config = get_special_themes_config(conf)
     calendar_config = get_calendar_config(conf)
